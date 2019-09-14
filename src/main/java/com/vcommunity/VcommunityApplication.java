bb0ef3444868388f180277e864c8b1bebf2ba613
mingV2019 <smw059184@163.com>
2019-09-14 23:54:49 +0800

    init repo

---
+++ src/main/java/com/vcommunity/VcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.vcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VcommunityApplication.class, args);
+    }
+
+}
