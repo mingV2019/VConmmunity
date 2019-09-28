bfb2fe3639a978a5ba49b2f1d36f24eea0d9d9cb
mingV2019 <smw059184@163.com>
2019-09-28 23:57:57 +0800

    9.28

---
+++ src/main/java/com/vcommunity/mapper/QuestionMapper.java
@@ -0,0 +1,17 @@
+package com.vcommunity.mapper;
+
+import com.vcommunity.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author ：mingV
+ * @description：
+ * @date ：Created in 2019/9/28 23:43
+ */
+@Mapper
+public interface QuestionMapper {
+
+    @Insert("insert into question (title,description.gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag}) ")
+    void create(Question question);
+}
